e5285ea83f336556257bb3b9d22b46b27b9a9bf8
Rajesh Kumar Yadav <itsyaboiinitial1ze@gmail.com>
2024-05-02 20:34:15 +0530

    fix filters buttons and container

--- src/components/FiltersContainer/FiltersContainer.tsx
+++ src/components/FiltersContainer/FiltersContainer.tsx
@@ -1,9 +1,49 @@
 import FilterButton from "../FilterButton/FilterButton";
+import "./FiltersContainer.css";
 
 const FiltersContainer = () => {
+    const buttonsData = [
+        {
+            purpose: "Roles",
+            options: [
+                "Software Engineer",
+                "Product Manager",
+                "Data Analyst",
+                "Data Scientist",
+                "UX Designer",
+            ],
+        },
+        {
+            purpose: "No Of Employees",
+            options: [
+                "1-10",
+                "11-20",
+                "21-50",
+                "51-100",
+                "101-200",
+                "201-500",
+                "500+",
+            ],
+        },
+        {
+            purpose: "Experience",
+            options: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        },
+        {
+            purpose: "Remote",
+            options: ["Remote", "Hybrid", "In-Office"],
+        },
+    ];
+
     return (
         <div className="filters-container">
-            <FilterButton></FilterButton>
+            {buttonsData.map((buttonData, index) => (
+                <FilterButton
+                    key={index}
+                    purpose={buttonData.purpose}
+                    options={buttonData.options}
+                />
+            ))}
         </div>
     );
 };
